83651b22d1091e07465a6f4b757f179254f61de6
IGhost-Of-PastI <95056441+IGhost-Of-PastI@users.noreply.github.com>
2024-02-25 13:18:35 +0300

    Добавьте файлы проекта.

---
+++ PR3.2/PR3.2.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+
+template <typename T, typename U, typename V>
+class MyClass {
+private:
+    T** array;
+    int rows;
+    int cols;
+    U* data1;
+    V* data2;
+
+public:
+    MyClass(int rows, int cols, U data1, V data2) : rows(rows), cols(cols) {
+        array = new T * [rows];
+        for (int i = 0; i < rows; ++i)
+            array[i] = new T[cols];
+
+        this->data1 = new U(data1);
+        this->data2 = new V(data2);
+    }
+
+    MyClass(const MyClass& other) : rows(other.rows), cols(other.cols) {
+        array = new T * [rows];
+        for (int i = 0; i < rows; ++i) {
+            array[i] = new T[cols];
+            for (int j = 0; j < cols; ++j)
+                array[i][j] = other.array[i][j];
+        }
+
+        data1 = new U(*other.data1);
+        data2 = new V(*other.data2);
+    }
+
+    MyClass& operator=(MyClass& other)
+    {
+        array = new T * [rows];
+        for (int i = 0; i < rows; ++i) {
+            array[i] = new T[cols];
+            for (int j = 0; j < cols; ++j)
+                array[i][j] = other.array[i][j];
+        }
+
+        data1 = new U(*other.data1);
+        data2 = new V(*other.data2);
+    }
+
+    ~MyClass() {
+        for (int i = 0; i < rows; ++i)
+            delete[] array[i];
+        delete[] array;
+
+        delete data1;
+        delete data2;
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+}
